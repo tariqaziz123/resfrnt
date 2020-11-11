25ce82a539a050a247e6b07b004a1881a0bbf9f9
Tariq Aziz <46774473+tariqaziz123@users.noreply.github.com>
2020-11-11 22:36:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ var port = process.env.PORT||9800;
 const path = require('path');
 
 
-const app=express();
 
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname,'build')));
+
 app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname, 'build/index.html'))
-})
+    res.sendFile(path.join(__dirname,'build/index.html'))
+});
 
-app.listen(port)
+
+
+app.listen(port,(err)=> {
+    if(err) throw err;
+    console.log(`Server is running on port ${port}`)
+})
